20d6d77d5dce5ac5b77ce9d1f35934156fe17e20
The boy <clifboycabrera12@gmail.com>
2025-06-17 14:22:14 +0800

    Update delete.js

--- delete.js
+++ delete.js
@@ -2,7 +2,7 @@ const runQuery = require('./connection');
 
 module.exports = async (users_id) => {
     try {
-        const query = 'DELETE FROM accounts WHERE users_id =?';
+        const query = 'DELETE FROM users WHERE users_id =?';
         const result = await runQuery(query, [users_id]);
         console.log('Account deleted:', result);
     } catch (error) {
